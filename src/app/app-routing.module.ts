119dce286a9dea0142697aa28a4c925bf06f7ce1
Asif Ansari <dawood@dawoodstudios.com>
2023-07-17 22:18:58 +0530

    url changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'style-guide', loadChildren: () => import('./style-guide/style-guide.module').then(m => m.StyleGuideModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./style-guide/style-guide.module').then(m => m.StyleGuideModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
